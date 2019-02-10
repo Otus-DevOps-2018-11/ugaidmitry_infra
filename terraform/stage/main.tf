c63c1d0642cd9b593a83f3166e44911df92788af
dimas <json1@yandex.ru>
2019-02-10 22:21:52 +0700

    HW10

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,3 +1,5 @@
+# Provider google
+
 provider "google" {
   version = "1.4.0"
   project = "${var.project}"
@@ -5,19 +7,24 @@ provider "google" {
 }
 
 module "app" {
-  source           = "../modules/app"
-  public_key_path  = "${var.public_key_path}"
-  zone             = "${var.zone}"
-  app_disk_image   = "${var.app_disk_image}"
-  app_machine_type = "${var.app_machine_type}"
+  source             = "../modules/app"
+  public_key_path    = "${var.public_key_path}"
+  zone               = "${var.zone}"
+  app_disk_image     = "${var.app_disk_image}"
+  private_key_path   = "${var.private_key_path}"
+  provision_var      = "${var.provision_var}"
+  files_deploy_sh    = "${var.files_deploy_sh}"
+  files_puma_service = "${var.files_puma_service}"
+  db_internal_ip     = "${module.db.db_internal_ip}"
 }
 
 module "db" {
-  source          = "../modules/db"
-  public_key_path = "${var.public_key_path}"
-  zone            = "${var.zone}"
-  db_disk_image   = "${var.db_disk_image}"
-  db_machine_type = "${var.db_machine_type}"
+  source           = "../modules/db"
+  public_key_path  = "${var.public_key_path}"
+  zone             = "${var.zone}"
+  db_disk_image    = "${var.db_disk_image}"
+  private_key_path = "${var.private_key_path}"
+  provision_var    = "${var.provision_var}"
 }
 
 module "vpc" {
